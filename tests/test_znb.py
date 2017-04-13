0b616d1fac2891af1f5607bc5334a3f791f72528
Lukas Sandström <lukas.sandstrom@rohde-schwarz.com>
2017-04-13 08:07:47 +0200

    Move tests for ZNB.Trace and ZNB.Channel from test2.py to tests/

--- tests/test_znb.py
+++ tests/test_znb.py
@@ -5,15 +5,38 @@
 """
 
 from .common import *
-import pytest
-from pytest import raises
-import inspect
-import logging
-import sys
-
 from RSSscpi import ZNB, ZVA
 
-logging.basicConfig(stream=sys.stdout)
+
+def test_channel(dummy_vna, visa):
+    """
+
+    :param ZNB dummy_vna:
+    :return:
+    """
+    ch = dummy_vna.get_channel(3)
+    x = ch.sweep.points.query_default()
+    assert isinstance(x, int)
+    ch.sweep.points.value = 301
+    x = ch.sweep.points.value
+    assert isinstance(x, int)
+    ch.sweep.points = 301
+    ch.sweep.TYPE.w("LIN")
+    ch.configure_freq_sweep(10, 10e6, points=101, ifbw=1e3, power=-10, log_sweep=True)
+    ch.init_sweep()
+    assert ["SENSe3:SWEep:POINts? DEF",
+            "SENSe3:SWEep:POINts 301",
+            "SENSe3:SWEep:POINts?",
+            "SENSe3:SWEep:POINts 301",
+            "SENSe3:SWEep:TYPE LIN",
+            "SENSe3:SWEep:TYPE LOG",
+            "SENSe3:FREQuency:STARt 10",
+            "SENSe3:FREQuency:STOP 10000000.0",
+            "SENSe3:SWEep:POINts 101",
+            "SENSe3:BANDwidth 1000.0",
+            "SOURce3:POWer:LEVel:IMMediate:AMPLitude -10",
+            "INITiate3:IMMediate",
+            ] == visa.cmd
 
 
 def test_marker(dummy_vna, visa):
@@ -55,5 +78,23 @@ def test_marker(dummy_vna, visa):
     assert ["CALCulate2:MARKer3:Y?"] == visa.cmd
     assert isinstance(x, float)
 
-    with raises(AttributeError, message="Assignment to marker y value is not possible."):
+    with pytest.raises(AttributeError, message="Assignment to marker y value is not possible."):
         m1.y = 2
+
+
+def test_trace(dummy_vna, visa):
+    """
+
+    :param ZNB dummy_vna:
+    :return:
+    """
+    ch = dummy_vna.get_channel(2)
+    tr = ch.get_trace("Tr7")
+    tr.ref_level = 8
+    x = tr.trace_format
+    tr.trace_format = 2
+    assert ["DISPlay:WINDow:TRACe:Y:SCALe:RLEVel 8, 'Tr7'",
+            "CALCulate2:PARameter:SELect 'Tr7'",
+            "CALCulate2:FORMat?",
+            "CALCulate2:FORMat 2",
+            ] == visa.cmd
--- tools/test2.py
+++ tools/test2.py
@@ -17,25 +17,6 @@ logging.basicConfig(stream=sys.stdout)
 znb = ZNB(DummyVisa("K1"))
 
 
-def test_trace():
-    print "*  test_trace()"
-    ch = znb.get_channel(2)
-    tr = ch.get_trace("Tr2")
-    x = tr.trace_format
-    tr.trace_format = 2
-
-
-def test_channel():
-    print "*  test_channel()"
-    ch = znb.get_channel(3)
-    ch.sweep.points.query_default()
-    ch.sweep.points.value = 301
-    ch.sweep.points = 301
-    ch.sweep.TYPE.w("LIN")
-    ch.configure_freq_sweep(10, 10e6, points=101, ifbw=1e3, power=-10, log_sweep=True)
-    ch.init_sweep()
-
-
 def test_diagram():
     print "*  test_diagram()"
     dia = znb.get_diagram(1)
@@ -45,7 +26,5 @@ def test_diagram():
 def test_sweep():
     pass
 
-test_channel()
-test_trace()
 test_diagram()
 
