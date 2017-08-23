96fb9f9c90276f3872a3409352715270cb3ead12
Lukas Sandström <lukas.sandstrom@rohde-schwarz.com>
2017-08-23 18:04:16 +0200

    Remove debugging print statement from tests/vna2.py

--- tests/vna2.py
+++ tests/vna2.py
@@ -19,7 +19,6 @@ class VNA2(VNA1):
 
     def __init__(self, visa_res):
         super(VNA2, self).__init__(visa_res)
-        print (self.A.__class__)
         self.bsub = Bsub2(parent=self.A)
 
     A = node("A", node("B", node("Ca", node("D"))))
