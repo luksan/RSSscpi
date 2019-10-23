7a3b11fa8bcd0887d487b4db536e1f239ff020d5
Lukas Sandström <lukas.sandstrom@rohde-schwarz.com>
2019-10-23 17:12:33 +0200

    zna: Add connect_ip() method

--- src/RSSscpi/zna/__init__.py
+++ src/RSSscpi/zna/__init__.py
@@ -1,8 +1,21 @@
 from RSSscpi.gen import ZNA_gen
 import RSSscpi.znb as znb
+from .. import network as net
 
 from .channel import Channel
 from .diagram import Diagram
+
+
+def connect_ip(ip_address: str) -> "ZNA":
+    """
+    Helper to connect to a ZNA VNA via TCPIP / HiSLIP / VISA.
+    Creates an ZNA instance and calls init() on it before returning.
+
+    :param ip_address: The ip address in string format
+    :return: An initialized ZNA instance.
+    :rtype: ZNA
+    """
+    return net.connect_ethernet(ZNA, ip_address, "hislip0")
 
 
 class ZNA(znb.ZNB):
